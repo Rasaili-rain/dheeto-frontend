0a568f4d0de83d720c2438d8e98c4d8771a6c3a3
Rasaili-rain <saditrasaili063@gmail.com>
2025-10-18 21:32:37 +0545

    minorr

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,20 +2,8 @@
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "../global.css";
-import React, { useLayoutEffect, useRef } from "react";
 
 export default function RootLayout() {
-  const hasMounted = useRef(false);
-  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
-
-  useLayoutEffect(() => {
-    if (hasMounted.current) return;
-    setIsColorSchemeLoaded(true);
-    hasMounted.current = true;
-  }, []);
-
-  if (!isColorSchemeLoaded) return null;
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Stack screenOptions={{ headerShown: false }}>
